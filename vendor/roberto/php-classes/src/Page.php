b4845b896c3197f14fdb41cd55b213ce8cba3d3e
Roberto <robertodotaruma@hotmail.com>
2023-01-02 20:44:32 +0100

    main page ainda bugada mas o resto funcionando

--- vendor/roberto/php-classes/src/Page.php
+++ vendor/roberto/php-classes/src/Page.php
@@ -10,47 +10,51 @@ class Page {
 	private $options = [];
 	private $defaults =
 	[
+		"header"=>true,
+		"footer"=>true,
 		"data" => []
 	];
 
-	public function __construct($options = array())
+	public function __construct($opts = array(), $tpl_dir = "/views/")
 	{
-		$this -> options = array_merge($this -> defaults, $options);
-		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+		$this -> options = array_merge($this -> defaults, $opts);
+		$config = array
+			(
+			"base_url"      => null,
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
 			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache",
 			"debug"         => false
 			);
 		Tpl::configure( $config );
 		$this -> tpl = new Tpl;
 
-		$this -> setData ($this -> options ["data"]);
+		if ($this->options['data']) $this -> setData ($this -> options ['data']);
 
-		$this -> tpl -> draw("header");
-
-
-	}
-
-	private function setData($data = array())
-	{
-		foreach ($this -> options ["data"] as $key => $value)
-		{
-			$this -> tpl -> assign ($key, $value);
-		}
-	}
-
-	public function setTpl($name, $data = array(), $returnHTML = false)
-	{
-		$this -> setData($data);
-
-		return $this -> tpl -> draw($name, $returnHTML);
+		if ($this -> options ['header'] ===true) $this -> tpl -> draw("header", false);
 	}
 
 	public function __destruct()
 	{
 
-		$this -> tpl -> draw ("footer");
+		if ($this -> options ["footer"] === true) $this -> tpl -> draw ("footer");
 	
 	}
+
+	private function setData($data = array())
+	{
+		foreach ($data as $key => $value)
+		{
+			$this -> tpl -> assign ($key, $value);
+		}
+	}
+
+	public function setTpl($tplname, $data = array(), $returnHTML = false)
+	{
+		$this -> setData($data);
+
+		return $this -> tpl -> draw($tplname, $returnHTML);
+	}
+
+
 
 }
