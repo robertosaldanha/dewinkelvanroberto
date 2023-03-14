2a89f97b2ecc08347ab1911fa2b3d745d9224e08
Roberto <robertodotaruma@hotmail.com>
2023-03-14 13:10:00 -0300

    Paginação das categorias

--- site.php
+++ site.php
@@ -16,17 +16,32 @@ $app -> get ('/', function()
 		]);	
 });
 
-//Rota para categoria
+//Rota para categoria com as páginas 
 $app -> get ("/categories/:idcategory", function($idcategory)
 	{
+		$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
 		$category = new Category();
 		$category -> get ((int)$idcategory);
+
+		$pagination = $category -> getProductsPage();
+
+		$pages = [];
+		for ($i=1; $i <= $pagination['pages']; $i++) 
+		{ 
+			array_push ($pages,
+				[
+					'link' => '/categories/' . $category -> getidcategory() . '?page=' . $i,
+					'page' => $i
+				]); 
+		}
 
 		$page = new Page();
 		$page -> setTpl("category",
 			[
 				'category' => $category -> getValues(),
-				'products' => Product::checklist($category -> getProducts())
+				'products' => $pagination ["data"],
+				'pages' => $pages
 			]);
 	});
 ?>
